c8cd84b0f92ecd7c902b7f1493831f6e906f9fd5
Samuel <36420837+Samueru-sama@users.noreply.github.com>
2026-03-29 04:07:28 -0400

    package app

--- make-appimage.sh
+++ make-appimage.sh
@@ -3,16 +3,19 @@
 set -eu
 
 ARCH=$(uname -m)
-VERSION=$(pacman -Q PACKAGENAME | awk '{print $2; exit}') # example command to get version of application here
+VERSION=$(pacman -Q waydroid | awk '{print $2; exit}') # example command to get version of application here
 export ARCH VERSION
 export OUTPATH=./dist
 export ADD_HOOKS="self-updater.bg.hook"
 export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}|latest|*$ARCH.AppImage.zsync"
-export ICON=PATH_OR_URL_TO_ICON
-export DESKTOP=PATH_OR_URL_TO_DESKTOP_ENTRY
+export ICON=/usr/share/icons/hicolor/512x512/apps/waydroid.png
+export DESKTOP=/usr/share/applications/Waydroid.desktop
 
 # Deploy dependencies
-quick-sharun /PATH/TO/BINARY_AND_LIBRARIES_HERE
+mkdir -p ./AppDir/bin
+cp -r /usr/lib/waydroid/* ./AppDir/bin
+ln -s waydroid.py ./AppDir/bin/waydroid
+quick-sharun ./AppDir/bin/*
 
 # Additional changes can be done in between here
 
