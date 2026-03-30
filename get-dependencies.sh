601a9673e76f8f0e80bce787c6eadecf502e49c2
Samuel <36420837+Samueru-sama@users.noreply.github.com>
2026-03-29 23:06:42 -0400

    add `12to11`

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -14,6 +14,7 @@ get-debloated-pkgs --add-common --prefer-nano
 
 # Comment this out if you need an AUR package
 make-aur-package zenity-rs-bin
+make-aur-package 12to11-git
 
 # If the application needs to be manually built that has to be done down here
 
